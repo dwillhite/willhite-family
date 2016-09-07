cbee5754ad3997d0cc545178faaa78307a4523fb
Dave Willhite <davidmwillhite@gmail.com>
2016-09-06 23:16:04 -0500

    added google maps

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,13 @@
 console.log("greatQ now get to work"),console.log("yes, its working"),console.log("yep, still working");
 
 document.getElementById('title').innerHTML = 'I am changed';
+$('.b').css("color", "white");
+$(document).ready(function(){
+    if (typeof jQuery == 'undefined') {
+
+        // jQuery IS NOT loaded, do stuff here.
+        console.log("jquerey is loading");
+
+    }
+
+});
